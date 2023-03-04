06084f67cb1d4af28844fbf6e2377071923dd066
Sarangkhandarkar <60329575+Sarangkhandarkar@users.noreply.github.com>
2023-03-04 23:40:07 +0530

    new upgrades in validations

--- bookmyturf/src/App.js
+++ bookmyturf/src/App.js
@@ -4,17 +4,20 @@ import Navbar from './components/common-components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Loginform from './components/Seperate-components/User/Loginform';
 import Registrationform from './components/Seperate-components/User/Registrationform';
-
+import ProductBox from './components/common-components/productbox';
+import { Card,Button } from 'react-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar/>
+    <div id='box'></div>
     <div className="container">
       <Routes>
         <Route path="/userlogin" element={<Loginform/>} />
         <Route path="/registeruser" element={<Registrationform/>} />
       </Routes>
+      
     </div>
     </BrowserRouter>
   );
--- bookmyturf/src/components/Seperate-components/User/Registrationform.js
+++ bookmyturf/src/components/Seperate-components/User/Registrationform.js
@@ -1,18 +1,17 @@
 import React,{useState} from 'react'
 import "./Loginform.css"
+import httpService from '../../http-service/http-service';
 //import apiurl from "./APIUrl.json"
-
-import axios from 'axios';
-let newdata ="";
+let newdata= "";
+// import axios from 'axios';
 function Registrationform() {
-  const url = "http://localhost:7070/students"
+  // const url = "http://localhost:7070/students"
   const [data,setData] =useState({
     firstname: "",
-    lastName  : "",
+    lastname: "",
     email: "",
     contactno: "",
-    password: "",
-    confirmpassword: ""
+    password: ""
   })
   // const [firstName , setFirstName] = useState("")
   // const [lastName , setLastName] = useState("")
@@ -22,81 +21,82 @@ function Registrationform() {
   // const [confirmpassword , setConfirmpassword] = useState("")
 
 function handle(e){
-  newdata = {...data}
+  let newdata = {...data}
   newdata[e.target.id] = e.target.value;
   setData(newdata);
 }
 
 function passMatch(e){
   if(data.password === e.target.value){
-    newdata[e.target.id] = e.target.value;
-    setData(newdata);
+    
   }else{
-    alert("password not match")
+    e.target.setCustomValidity("password should match");
+    //alert("password not match")
   }
-  
 }
 
-function submit(e){
-    e.preventDefault();
-    axios.post(url,{
-      firstname: data.firstname,
-    lastName  : data.lastName,
-    email: data.email,
-    contactno: data.contactno,
-    password: data.password,
-    confirmpassword: data.confirmpassword
-    }).then(resp=>{
-      console.log(resp.data)
-    })
+// function submit(e){
+//     e.preventDefault();
+//     axios.post(url,{
+//       firstname: data.firstname,
+//       lastname  : data.lastname,
+//       email: data.email,
+//       contactno: data.contactno,
+//       password: data.password,
+//       confirmpassword: data.confirmpassword
+//     }).then(resp=>{
+//       console.log(resp.data)
+//     })
 
-    
+function submit(e){
+  e.preventDefault();
+  httpService.registeruser(data).then(res => console.log(res.data));
 }
   return (
     <div>
         <center><h1>User Registration</h1></center>
-      <form className=" userform row " onSubmit={(e) => submit(e)}>
+      <form autocomplete="off" className=" userform row " onSubmit={(e) => submit(e)}>
      
         
 
       <div className="form-group row">
-                <label for="firstname" className='col-sm-2 col-form-label' >First Name:</label>
+                <label htmlFor="firstname" className='col-sm-2 col-form-label' >First Name:</label>
                   <div className="col-sm-10">
-                  <input className='form-control' onChange={(e)=>handle(e)} type="text" id="firstname" name="firstname" required /><br/>
+                  <input className='form-control' onBlur={(e)=>handle(e)} type="text" id="firstname" name="firstname" required /><br/>
                   </div>
       </div>
 
       <div className="form-group row">
-                <label for="lastname" className='col-sm-2 col-form-label'>Last Name:</label>
+                <label htmlFor="lastname" className='col-sm-2 col-form-label'>Last Name:</label>
                   <div className="col-sm-10">
-                  <input className='form-control' onChange={(e)=>handle(e)} type="text" id="lastname" name="lastname" required/><br/>
+                  <input className='form-control' onBlur={(e)=>handle(e)} type="text" id="lastname" name="lastname" required/><br/>
                   </div>
       </div>
 
       <div className="form-group row">
-                  <label for="email" className='col-sm-2 col-form-label'>Email:</label>
+                  <label htmlFor="email" className='col-sm-2 col-form-label'>Email:</label>
                   <div className="col-sm-10">
-                  <input className='form-control' onChange={(e)=>handle(e)}  type="text" id="email" name="email" required /><br/>
+                  <input className='form-control' onBlur={(e)=>handle(e)}  type="text" id="email" name="email" required /><br/>
                   </div>
       </div>
 
       <div className="form-group row">
-      <label for="contactno" className='col-sm-2 col-form-label'>Contact Number:</label>
+      <label htmlFor="contactno" className='col-sm-2 col-form-label'>Contact Number:</label>
                   <div className="col-sm-10">
-                  <input className='form-control'type="tel" onChange={(e)=>handle(e)} id="contactno" name="contactno" required/><br/>
+                  <input className='form-control'type="tel" onBlur={(e)=>handle(e)} id="contactno" name="contactno" required/><br/>
 
                   </div>
       </div>
 
       <div className="form-group row">
-                 <label for="password" className='col-sm-2 col-form-label'>Password:</label>
+                 <label htmlFor="password" className='col-sm-2 col-form-label'>Password:</label>
                   <div className="col-sm-10">
-                  <input className='form-control'type="password" onChange={(e)=>handle(e)} id="password" name="password" required/><br/>
+                  <input className='form-control'type="password" onBlur={(e)=>handle(e)} id="password" name="password" required/><br/>
                   </div>
       </div>
 
       <div className="form-group row">
-                  <label for="confirmpassword" className='col-sm-2 col-form-label'>Confirm Password:</label>
+                  <label htmlFor="confirmpassword" className='col-sm-2 col-form-label'>Confirm Password:</label>
                   <div className="col-sm-10">
                   <input className='form-control'type="password" onBlur={(e)=>passMatch(e)} id="confirmpassword" name="confirmpassword" required/><br/>
                   </div>
@@ -105,12 +105,12 @@ function submit(e){
           <label className="col-form-label col-sm-2 ">Role</label>
               <div className="col-sm-10">
                     <div className="form-check">
-                        <input className="form-check-input" type="radio" name="userRole" id="gridRadios1" value="turf"  />
-                        <label className="form-check-label" for="gridRadios1">Turf Owner</label>
+                        <input className="form-check-input" type="radio" name="userRole" id="gridRadios1" value="turf" defaultChecked />
+                        <label className="form-check-label" htmlFor="gridRadios1">Turf Owner</label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" type="radio" name="userRole" id="gridRadios2" value="player" checked />
-                        <label className="form-check-label" for="gridRadios2">Players</label>
+                        <input className="form-check-input" type="radio" name="userRole" id="gridRadios2" value="player" />
+                        <label className="form-check-label" htmlFor="gridRadios2">Players</label>
                     </div>
               </div>
       </div>
