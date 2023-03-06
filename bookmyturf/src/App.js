499d1b3ae5cf1ed0f74120767f9c19927e5c953b
Sarangkhandarkar <60329575+Sarangkhandarkar@users.noreply.github.com>
2023-03-06 21:29:50 +0530

    todays commit before pull

--- bookmyturf/src/App.js
+++ bookmyturf/src/App.js
@@ -4,12 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Loginform from './components/Seperate-components/User/Loginform';
 import Registrationform from './components/Seperate-components/User/Registrationform';
 import AddTurf from './components/Seperate-components/Turf/AddTurf';
-<<<<<<< HEAD
-import AddSport from './components/Seperate-components/Sport/AddSport'
-=======
 import Homepage from './components/common-components/Homepage'
 import i1 from './images/image1.jpg'
->>>>>>> ac606a109046b8ecfa58edbb1a87de0289d348dc
 
 function App() {
   return (
@@ -21,11 +17,7 @@ function App() {
         <Route path="/userLogin" element={<Loginform/>} />
         <Route path="/registeruser" element={<Registrationform/>} />
         <Route path="/addturf" element={<AddTurf/>} />
-<<<<<<< HEAD
-        <Route path="/addsport" element={<AddSport/>} />
-=======
         <Route path="/" element={<Homepage/>} />
->>>>>>> ac606a109046b8ecfa58edbb1a87de0289d348dc
       </Routes>
     </div>
     </BrowserRouter>
