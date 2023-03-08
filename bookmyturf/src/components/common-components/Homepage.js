3c0efda9a9df89ca40763b496c068c39e1b28070
PatelKrunali <pkrunali1412@gmail.com>
2023-03-08 17:00:26 +0530

    courosel added

--- bookmyturf/src/components/common-components/Homepage.js
+++ bookmyturf/src/components/common-components/Homepage.js
@@ -7,8 +7,7 @@ import '../Seperate-components/Dashboard/Card.css'
 export default function Homepage() {
   return (
     <>
-  
-<div id="carouselExampleCaptions" class="carousel slide">
+  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" carousel-transition-duration="0.05">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -25,9 +24,10 @@ export default function Homepage() {
       <div className="card text-bg-dark col-sm-7 mt-5 text-center mx-auto " >
   <img src="g1.gif" className="img-fluid rounded-start " id='imagecontainer' alt="..."/>
   <div className="card-img-overlay  ">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+   
+    <h4 className="card-text mx-auto mt-5">The five S’s of sports training are:  </h4>
+    <h3 className="card-text  mx-auto mt-5"> stamina, speed, strength, skill, and spirit;</h3>
+    <h2 className="card-text  mx-auto mt-5">but the greatest of these is spirit.</h2>
   </div>
   </div>
   </div>
@@ -35,11 +35,13 @@ export default function Homepage() {
 
     <div class="carousel-item">
     <div className="card text-bg-dark col-sm-7 mt-5 text-center mx-auto" >
+  
   <img src="f10.jpeg" className="img-fluid rounded-start " id='imagecontainer' alt="..."/>
   <div className="card-img-overlay "id='Homepage-Container'>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+
+    <h1 className="card-title">Cricket</h1>
+    <h3 className="card-text">Life is short, Play Hard!!!</h3>
+    
   </div>
   </div>
     </div>
@@ -47,11 +49,11 @@ export default function Homepage() {
 
     <div class="carousel-item">
     <div className="card text-bg-dark col-sm-7 mt-5 text-center mx-auto" >
-  <img src="f20.jpeg" className="img-fluid rounded-start " id='imagecontainer' alt="..."/>
+  <img src="f2.jpeg" className="img-fluid rounded-start " id='imagecontainer' alt="..."/>
   <div className="card-img-overlay "id='Homepage-Container'>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+    <h1 className="card-title">FootBall</h1>
+    <h3 className="card-text">Don’t just chase your dreams…run them down!</h3>
+    
   </div>
   </div>
     </div>
@@ -59,11 +61,11 @@ export default function Homepage() {
 
     <div class="carousel-item">
     <div className="card text-bg-dark col-sm-7 mt-5 text-center mx-auto" >
-  <img src="H1.jpg" className="img-fluid rounded-start " id='imagecontainer' alt="..."/>
+  <img src="H1.jpg" className="img-fluid rounded-start  opacity-50" id='imagecontainer' alt="..."/>
   <div className="card-img-overlay "id='Homepage-Container'>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+    <h1 className="card-title">Hockey</h1>
+    <h3 className="card-text">For the love of the Game.</h3>
+    
   </div>
   </div>
     </div>
@@ -71,11 +73,11 @@ export default function Homepage() {
 
     <div class="carousel-item">
     <div className="card text-bg-dark col-sm-7 mt-5 text-center mx-auto" >
-  <img src="tt1.jpg" className="img-fluid rounded-start " id='imagecontainer' alt="..."/>
+  <img src="tt1.jpg" className="img-fluid rounded-start opacity-50 " id='imagecontainer' alt="..."/>
   <div className="card-img-overlay "id='Homepage-Container'>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+    <h1 className="card-title">Table Tennis</h1>
+    <h3 className="card-text">Get your Game on.</h3>
+   
   </div>
   </div>
     </div>
@@ -83,43 +85,42 @@ export default function Homepage() {
 
     <div class="carousel-item">
     <div className="card text-bg-dark col-sm-7 mt-5 text-center mx-auto" >
-  <img src="T1.jpg" className="img-fluid rounded-start " id='imagecontainer' alt="..."/>
+  <img src="T1.jpg" className="img-fluid rounded-start opacity-50 " id='imagecontainer' alt="..."/>
   <div className="card-img-overlay "id='Homepage-Container'>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+    <h1 className="card-title">Tennis</h1>
+    <h3 className="card-text">Don’t race against others, race against yourself!</h3>
+   
   </div>
   </div>
     </div>
  
 
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
-    <span class="visually-hidden text-dark">Previous</span>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
   </button>
 
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 
